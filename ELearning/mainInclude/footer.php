bbdb089a64b4b709d5f52f06d035c9c4e95950e6
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2024-11-13 17:46:08 +0800

    Changing Sign up and Login

--- ELearning/mainInclude/footer.php
+++ ELearning/mainInclude/footer.php
@@ -1,53 +1,53 @@
  <!-- Start Footer -->
  <footer class="container-fluid bg-dark text-center p-2">
-    <small class="text-white"> &copy; || Group  || <?php   
-          if (isset($_SESSION['is_admin_login'])){
-            echo '<a href="admin/adminDashboard.php"> Admin Dashboard</a> <a href="logout.php">Logout</a>';
-          }else {
-            echo '<a href="#login" data-toggle="modal" data-target="#adminLoginModalCenter"> Admin Login</a>';
-          }
-    ?>
-  </small> 
-  
+   <small class="text-white"> &copy; || Group || <?php
+                                                  if (isset($_SESSION['is_admin_login'])) {
+                                                    echo '<a href="admin/adminDashboard.php"> Admin Dashboard</a> <a href="logout.php">Logout</a>';
+                                                  } else {
+                                                    echo '<a href="#login" data-toggle="modal" data-target="#adminLoginModalCenter"> Admin Login</a>';
+                                                  }
+                                                  ?>
+   </small>
+
  </footer> <!-- End Footer -->
 
-    <!-- Start Student Registration Modal -->
-    <div class="modal fade" id="stuRegModalCenter" tabindex="-1" role="dialog" aria-labelledby="stuRegModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="stuRegModalCenterTitle">Student Registration</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="clearAllStuReg()">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <!--Start Form Registration-->
-            <?php include('studentRegistration.php'); ?>
-            <!-- End Form Registration -->
-          </div>
-          <div class="modal-footer">
+ <!-- Start Student Registration Modal -->
+ <div class="modal fade" id="stuRegModalCenter" tabindex="-1" role="dialog" aria-labelledby="stuRegModalCenterTitle" aria-hidden="true">
+   <div class="modal-dialog modal-dialog-centered" role="document">
+     <div class="modal-content">
+       <div class="modal-header" style="border:none;">
+         <!-- <h5 class="modal-title" id="stuRegModalCenterTitle">Student Registration</h5> -->
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="clearAllStuReg()">
+           <span aria-hidden="true">&times;</span>
+         </button>
+       </div>
+       <div class="modal-body p4">
+         <!--Start Form Registration-->
+         <?php include('studentRegistration.php'); ?>
+         <!-- End Form Registration -->
+       </div>
+       <!-- <div class="modal-footer">
             <span id="successMsg"></span>
             <button type="button" class="btn btn-primary" id="signup" onclick="addStu()">Sign Up</button>
             <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="clearAllStuReg()">Close</button>
-          </div>
-        </div>
-      </div>
-    </div> <!-- End Student Registration Modal -->
+          </div> -->
+     </div>
+   </div>
+ </div> <!-- End Student Registration Modal -->
 
 
-    <!-- Start Student Login Modal -->
-    <div class="modal fade" id="stuLoginModalCenter" tabindex="-1" role="dialog" aria-labelledby="stuLoginModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="stuLoginModalCenterTitle">Student Login</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close" onClick="clearStuLoginWithStatus()">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form role="form" id="stuLoginForm">
+ <!-- Start Student Login Modal -->
+ <div class="modal fade" id="stuLoginModalCenter" tabindex="-1" role="dialog" aria-labelledby="stuLoginModalCenterTitle" aria-hidden="true">
+   <div class="modal-dialog modal-dialog-centered" role="document">
+     <div class="modal-content">
+       <div class="modal-header" style="border:none;">
+         <!-- <h5 class="modal-title" id="stuLoginModalCenterTitle">Student Login</h5> -->
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close" onClick="clearStuLoginWithStatus()">
+           <span aria-hidden="true">&times;</span>
+         </button>
+       </div>
+       <div class="modal-body">
+         <!-- <form role="form" id="stuLoginForm">
               <div class="form-group">
                 <i class="fas fa-envelope"></i><label for="stuLogEmail" class="pl-2 font-weight-bold">Email</label><small id="statusLogMsg1"></small><input type="email"
                     class="form-control" placeholder="Email" name="stuLogEmail" id="stuLogEmail">
@@ -55,69 +55,106 @@
                 <div class="form-group">
                   <i class="fas fa-key"></i><label for="stuLogPass" class="pl-2 font-weight-bold">Password</label><input type="password" class="form-control" placeholder="Password" name="stuLogPass" id="stuLogPass">
               </div>
-            </form>
-          </div>
-          <div class="modal-footer">
+            </form> -->
+
+         <link rel="stylesheet" href="./css/modal.css">
+         <div class="d-flex align-items-center justify-content-center text-center mb-3">
+           <img src="https://neust.edu.ph/wp-content/uploads/2020/06/neust_logo-1.png" width="100px" />
+           <!-- <span class="h1 fw-bold mb-0">NEUST</span> -->
+         </div>
+
+         <div class="row justify-content-center">
+           <div class="col-md-7">
+
+             <h5 class="fw-normal mb-3 pb-3 text-center" style="letter-spacing: 1px; font-size:24px;">Sign into your account</h5>
+
+             <form role="form" id="stuRegForm">
+               <div class="form-outline mb-4">
+                 <label class="form-label" for="stuLogEmail" >Name</label>
+                 <input type="text" class="form-control form-control-lg" style="height:40px; font-size:16px;" placeholder="ex: Dela Cruz"name="stuLogEmail" id="stuLogEmail">
+               </div>
+
+               <div class="form-outline mb-4">
+                 <label class="form-label" for="stuLogPass" >Password</label>
+                 <input type="password" class="form-control form-control-lg" style="height:40px; font-size:16px;" placeholder="Password" name="stuLogPass" id="stuLogPass">
+                 
+               </div>
+               
+               <button type="button" class="btn btn-primary btn-lg btn-block" style="height:40px; font-size:16px;"  id="stuLoginBtn" onclick="checkStuLogin()">Login</button>
+
+               <small id="statusLogMsg" class="row justify-content-center mt-3"></small>
+             </form>
+           </div>
+         </div>
+
+         <div class="text-center mt-4">
+           <a href="#!" class="small text-muted">Terms of use.</a>
+           <a href="#!" class="small text-muted mx-3">Privacy policy</a>
+         </div>
+
+
+       </div>
+       <!-- <div class="modal-footer">
             <small id="statusLogMsg"></small>
             <button type="button" class="btn btn-primary" id="stuLoginBtn" onclick="checkStuLogin()">Login</button>
             <button type="button" class="btn btn-secondary" data-dismiss="modal" onClick="clearStuLoginWithStatus()">Cancel</button>
-          </div>
-        </div>
-      </div>
-    </div> <!-- End Student Login Modal -->
+          </div> -->
+     </div>
+   </div>
+ </div> <!-- End Student Login Modal -->
 
 
-  <!-- Start Admin Login Modal -->
-  <div class="modal fade" id="adminLoginModalCenter" tabindex="-1" role="dialog" aria-labelledby="adminLoginModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="adminLoginModalCenterTitle">Admin Login</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close" onClick="clearAdminLoginWithStatus()">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form role="form" id="adminLoginForm">
-              <div class="form-group">
-                <i class="fas fa-envelope"></i><label for="adminLogEmail" class="pl-2 font-weight-bold">Email</label><input type="email"
-                    class="form-control" placeholder="Email" name="adminLogEmail" id="adminLogEmail">
-                </div>
-                <div class="form-group">
-                  <i class="fas fa-key"></i><label for="adminLogPass" class="pl-2 font-weight-bold">Password</label><input type="password" class="form-control" placeholder="Password" name="adminLogPass" id="adminLogPass">
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <small id="statusAdminLogMsg"></small>
-            <button type="button" class="btn btn-primary" id="adminLoginBtn" onclick="checkAdminLogin()">Login</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal" onClick="clearAdminLoginWithStatus()">Cancel</button>
-          </div>
-        </div>
-      </div>
-    </div> <!-- End Admin Login Modal -->
+ <!-- Start Admin Login Modal -->
+ <div class="modal fade" id="adminLoginModalCenter" tabindex="-1" role="dialog" aria-labelledby="adminLoginModalCenterTitle" aria-hidden="true">
+   <div class="modal-dialog modal-dialog-centered" role="document">
+     <div class="modal-content">
+       <div class="modal-header">
+         <h5 class="modal-title" id="adminLoginModalCenterTitle">Admin Login</h5>
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close" onClick="clearAdminLoginWithStatus()">
+           <span aria-hidden="true">&times;</span>
+         </button>
+       </div>
+       <div class="modal-body">
+         <form role="form" id="adminLoginForm">
+           <div class="form-group">
+             <i class="fas fa-envelope"></i><label for="adminLogEmail" class="pl-2 font-weight-bold">Email</label><input type="email"
+               class="form-control" placeholder="Email" name="adminLogEmail" id="adminLogEmail">
+           </div>
+           <div class="form-group">
+             <i class="fas fa-key"></i><label for="adminLogPass" class="pl-2 font-weight-bold">Password</label><input type="password" class="form-control" placeholder="Password" name="adminLogPass" id="adminLogPass">
+           </div>
+         </form>
+       </div>
+       <div class="modal-footer">
+         <small id="statusAdminLogMsg"></small>
+         <button type="button" class="btn btn-primary" id="adminLoginBtn" onclick="checkAdminLogin()">Login</button>
+         <button type="button" class="btn btn-secondary" data-dismiss="modal" onClick="clearAdminLoginWithStatus()">Cancel</button>
+       </div>
+     </div>
+   </div>
+ </div> <!-- End Admin Login Modal -->
 
-    <!-- Jquery and Boostrap JavaScript -->
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/popper.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+ <!-- Jquery and Boostrap JavaScript -->
+ <script type="text/javascript" src="js/jquery.min.js"></script>
+ <script type="text/javascript" src="js/popper.min.js"></script>
+ <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
-    <!-- Font Awesome JS -->
-    <script type="text/javascript" src="js/all.min.js"></script>
+ <!-- Font Awesome JS -->
+ <script type="text/javascript" src="js/all.min.js"></script>
 
-    <!-- Student Testimonial Owl Slider JS  -->
-    <script type="text/javascript" src="js/owl.min.js"></script>
-    <script type="text/javascript" src="js/testyslider.js"></script>
+ <!-- Student Testimonial Owl Slider JS  -->
+ <script type="text/javascript" src="js/owl.min.js"></script>
+ <script type="text/javascript" src="js/testyslider.js"></script>
 
-    <!-- Student Ajax Call JavaScript -->
-    <script type="text/javascript" src="js/ajaxrequest.js"></script>
+ <!-- Student Ajax Call JavaScript -->
+ <script type="text/javascript" src="js/ajaxrequest.js"></script>
 
-    <!-- Admin Ajax Call JavaScript -->
-    <script type="text/javascript" src="js/adminajaxrequest.js"></script>
+ <!-- Admin Ajax Call JavaScript -->
+ <script type="text/javascript" src="js/adminajaxrequest.js"></script>
 
-    <!-- Custom JavaScript -->
-    <script type="text/javascript" src="js/custom.js"></script>
+ <!-- Custom JavaScript -->
+ <script type="text/javascript" src="js/custom.js"></script>
 
-  </body>
+ </body>
 
-</html>
+ </html>
--- ELearning/studentRegistration.php
+++ ELearning/studentRegistration.php
@@ -1,18 +1,35 @@
-
-
 <link rel="stylesheet" href="./css/modal.css">
-<form role="form" id="stuRegForm">
-   <div class="form-group">
-     <i class="fas fa-user"></i><label for="stuname" class="pl-2 font-weight-bold">Name</label><small id="statusMsg1"></small><input type="text"
-       class="form-control" placeholder="Name" name="stuname" id="stuname">
-   </div>
-   <div class="form-group">
-   <i class="fas fa-envelope"></i><label for="stuemail" class="pl-2 font-weight-bold">Email</label><small id="statusMsg2"></small><input type="email"
-       class="form-control" placeholder="Email" name="stuemail" id="stuemail">
-     <!-- <small class="form-text">We'll never share your email with anyone else.</small> -->
-   </div>
-   <div class="form-group">
-     <i class="fas fa-key"></i><label for="stupass" class="pl-2 font-weight-bold">
-       Password</label><small id="statusMsg3"></small><input type="password" class="form-control" placeholder="Password" name="stupass" id="stupass">
-   </div>
- </form>
+<div class="d-flex align-items-center justify-content-center text-center mb-3">
+  <img src="https://neust.edu.ph/wp-content/uploads/2020/06/neust_logo-1.png" width="100px" />
+  <!-- <span class="h1 fw-bold mb-0">NEUST</span> -->
+</div>
+
+<div class="row justify-content-center">
+  <div class="col-md-7">
+    <h5 class="fw-normal mb-3 pb-3 text-center" style="letter-spacing: 1px; font-size:24px;">Sign Up</h5>
+
+    <form role="form" id="stuRegForm">
+      <div class="form-outline mb-4">
+        <label class="form-label" for="stuname">Name</label>
+        <input type="text" class="form-control form-control-lg" style="height:40px; font-size:16px;" placeholder="ex: Dela Cruz" name="stuname" id="stuname">
+      </div>
+
+      <div class="form-outline mb-4">
+        <label class="form-label" for="stuemail">Email</label>
+        <input type="email" class="form-control form-control-lg" style="height:40px; font-size:16px;" placeholder="Email" name="stuemail" id="stuemail">
+      </div>
+
+      <div class="form-outline mb-4">
+        <label class="form-label" for="stupass">New Password</label>
+        <input type="password" class="form-control form-control-lg" style="height:40px; font-size:16px;" placeholder="Password" name="stupass" id="stupass">
+      </div>
+
+      <button type="button" class="btn btn-primary btn-lg btn-block" style="height:40px; font-size:16px;" id="signup" onclick="addStu()">Sign Up</button>
+    </form>
+  </div>
+</div>
+
+<div class="text-center mt-4">
+  <a href="#!" class="small text-muted">Terms of use.</a>
+  <a href="#!" class="small text-muted mx-3">Privacy policy</a>
+</div>
