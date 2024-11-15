6997576aaab3a0d4c6fcbe1699c1e10091065969
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2024-11-15 16:15:02 +0800

    Updated header
    changing the header with glassmorphism and putting some bento box

--- ELearning/index.php
+++ ELearning/index.php
@@ -33,6 +33,9 @@ if (!isset($_SESSION['is_login'])) {
 <!-- Course Display Logic -->
 <?php
 if (isset($_SESSION['is_login'])) {
+
+
+  
     if (isset($_GET['view_all']) && $_GET['view_all'] == 'true') {
         $sql = "SELECT * FROM course";
         $buttonText = "Limit View";
@@ -42,14 +45,66 @@ if (isset($_SESSION['is_login'])) {
     }
 
     $result = $conn->query($sql);
+    
 
-    echo '<div class="container-fluid py-5 my-5">
-            <div class="row">';
+    echo '<div class="container-fluid py-5 my-5">';
+
+    echo '<div class="row mt-4 mb-5">
+    <div class="col-lg-7 mb-lg-0 mb-4">
+      <div class="card">
+        <div class="card-body p-3">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="d-flex flex-column h-100">
+                <p class="mb-1 pt-2 text-bold">NEUST E-Learning</p>
+                <h5 class="font-weight-bolder">E Learning</h5>
+                <p class="mb-5">From colors, cards, typography to complex elements, you will find the full documentation.</p>
+                <a class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="javascript:;">
+                  Read More
+                  <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+            <div class="col-lg-5 ms-auto text-center mt-5 mt-lg-0">
+              <div class="border-radius-lg h-100" style="background-color:#252525; border-radius: 10px;">
+        
+                <div class="position-relative d-flex align-items-center justify-content-center h-100">
+               
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>';
+
+    echo '<div class="col-lg-5 mb-5">
+          <div class="card h-100 p-3">
+            <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100" style="background-color: #252525; border-radius: 10px;">
+              <span class="mask bg-gradient-dark"></span>
+              <div class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3">
+                <h5 class="text-white font-weight-bolder mb-4 pt-2">Work with the rockets</h5>
+                <p class="text-white">Wealth creation is an evolutionarily recent positive-sum game. It is all about who take the opportunity first.</p>
+                <a class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="javascript:;">
+                  Read More
+                  <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> ';
+
+    
+      echo ' <div class="row">';
+
+            
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $course_id = $row['course_id'];
             $instructor_id = $row['instructor_id'];
+
 
             // Fetch instructor details
             $instructor_sql = "SELECT * FROM instructor WHERE instructor_id = '$instructor_id'";
@@ -62,8 +117,8 @@ if (isset($_SESSION['is_login'])) {
                 <div class="card" style="height:400px">
                     <img src="' . str_replace('..', '.', $row['course_img']) . '" class="card-img-top" alt="Course Image"/>
                     <div class="card-body">
-                        <h5 class="card-title">' . $row['course_name'] . '</h5>
-                        <p class="card-text">' . $row['course_desc'] . '</p>
+                        <h5 class="card-title" style="color: #252525; font-weight:bold;">' . $row['course_name'] . '</h5>
+                        <p class="card-text" style="color: #252525;">' . $row['course_desc'] . '</p>
                     </div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
                         <div>
@@ -82,7 +137,7 @@ if (isset($_SESSION['is_login'])) {
 
     // Button for viewing all courses
     echo '<div class="text-center m-4">
-            <a class="btn btn-danger btn-sm btns" href="index.php?view_all=' . (isset($_GET['view_all']) && $_GET['view_all'] == 'true' ? 'false' : 'true') . '">' . $buttonText . '</a>
+            <a class="btn btn-sm btns" href="index.php?view_all=' . (isset($_GET['view_all']) && $_GET['view_all'] == 'true' ? 'false' : 'true') . '">' . $buttonText . '</a>
           </div>';
 }
 ?>
@@ -127,6 +182,200 @@ if (!isset($_SESSION['is_login'])) {
   </div> ';
 }
 ?>
+
+
+
+
+<script src="../js/popper.min.js"></script>
+                <script src="../js/bootstrap.min.js"></script>
+                <script src="../js/perfect-scrollbar.min.js"></script>
+                <script src="../js/smooth-scrollbar.min.js"></script>
+                <script src="../js/chartjs.min.js"></script>
+                <script>
+                    var ctx = document.getElementById("chart-bars").getContext("2d");
+
+                    new Chart(ctx, {
+                        type: "bar",
+                        data: {
+                            labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                            datasets: [{
+                                label: "Sales",
+                                tension: 0.4,
+                                borderWidth: 0,
+                                borderRadius: 4,
+                                borderSkipped: false,
+                                backgroundColor: "#fff",
+                                data: [450, 200, 100, 220, 500, 100, 400, 230, 500],
+                                maxBarThickness: 6
+                            }, ],
+                        },
+                        options: {
+                            responsive: true,
+                            maintainAspectRatio: false,
+                            plugins: {
+                                legend: {
+                                    display: false,
+                                }
+                            },
+                            interaction: {
+                                intersect: false,
+                                mode: 'index',
+                            },
+                            scales: {
+                                y: {
+                                    grid: {
+                                        drawBorder: false,
+                                        display: false,
+                                        drawOnChartArea: false,
+                                        drawTicks: false,
+                                    },
+                                    ticks: {
+                                        suggestedMin: 0,
+                                        suggestedMax: 500,
+                                        beginAtZero: true,
+                                        padding: 15,
+                                        font: {
+                                            size: 14,
+                                            family: "Inter",
+                                            style: 'normal',
+                                            lineHeight: 2
+                                        },
+                                        color: "#fff"
+                                    },
+                                },
+                                x: {
+                                    grid: {
+                                        drawBorder: false,
+                                        display: false,
+                                        drawOnChartArea: false,
+                                        drawTicks: false
+                                    },
+                                    ticks: {
+                                        display: false
+                                    },
+                                },
+                            },
+                        },
+                    });
+
+
+                    var ctx2 = document.getElementById("chart-line").getContext("2d");
+
+                    var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+                    gradientStroke1.addColorStop(1, 'rgba(203,12,159,0.2)');
+                    gradientStroke1.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+                    gradientStroke1.addColorStop(0, 'rgba(203,12,159,0)'); //purple colors
+
+                    var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+                    gradientStroke2.addColorStop(1, 'rgba(20,23,39,0.2)');
+                    gradientStroke2.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+                    gradientStroke2.addColorStop(0, 'rgba(20,23,39,0)'); //purple colors
+
+                    new Chart(ctx2, {
+                        type: "line",
+                        data: {
+                            labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                            datasets: [{
+                                    label: "Mobile apps",
+                                    tension: 0.4,
+                                    borderWidth: 0,
+                                    pointRadius: 0,
+                                    borderColor: "#cb0c9f",
+                                    borderWidth: 3,
+                                    backgroundColor: gradientStroke1,
+                                    fill: true,
+                                    data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+                                    maxBarThickness: 6
+
+                                },
+                                {
+                                    label: "Websites",
+                                    tension: 0.4,
+                                    borderWidth: 0,
+                                    pointRadius: 0,
+                                    borderColor: "#3A416F",
+                                    borderWidth: 3,
+                                    backgroundColor: gradientStroke2,
+                                    fill: true,
+                                    data: [30, 90, 40, 140, 290, 290, 340, 230, 400],
+                                    maxBarThickness: 6
+                                },
+                            ],
+                        },
+                        options: {
+                            responsive: true,
+                            maintainAspectRatio: false,
+                            plugins: {
+                                legend: {
+                                    display: false,
+                                }
+                            },
+                            interaction: {
+                                intersect: false,
+                                mode: 'index',
+                            },
+                            scales: {
+                                y: {
+                                    grid: {
+                                        drawBorder: false,
+                                        display: true,
+                                        drawOnChartArea: true,
+                                        drawTicks: false,
+                                        borderDash: [5, 5]
+                                    },
+                                    ticks: {
+                                        display: true,
+                                        padding: 10,
+                                        color: '#b2b9bf',
+                                        font: {
+                                            size: 11,
+                                            family: "Inter",
+                                            style: 'normal',
+                                            lineHeight: 2
+                                        },
+                                    }
+                                },
+                                x: {
+                                    grid: {
+                                        drawBorder: false,
+                                        display: false,
+                                        drawOnChartArea: false,
+                                        drawTicks: false,
+                                        borderDash: [5, 5]
+                                    },
+                                    ticks: {
+                                        display: true,
+                                        color: '#b2b9bf',
+                                        padding: 20,
+                                        font: {
+                                            size: 11,
+                                            family: "Inter",
+                                            style: 'normal',
+                                            lineHeight: 2
+                                        },
+                                    }
+                                },
+                            },
+                        },
+                    });
+                </script>
+                <script>
+                    var win = navigator.platform.indexOf('Win') > -1;
+                    if (win && document.querySelector('#sidenav-scrollbar')) {
+                        var options = {
+                            damping: '0.5'
+                        }
+                        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+                    }
+                </script>
+                <!-- Github buttons -->
+                <script async defer src="https://buttons.github.io/buttons.js"></script>
+                <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+                <script src="../assets/js/soft-ui-dashboard.min.js?v=1.1.0"></script>
+
+
 
 
 
--- ELearning/mainInclude/header.php
+++ ELearning/mainInclude/header.php
@@ -1,39 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    
+
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" type="text/css" href="css/all.min.css">
-    
+
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-    
+
     <!-- Custom Style CSS -->
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <link id="pagestyle" href="../css/soft-ui-dashboard.css?v=1.1.0" rel="stylesheet" />
     <title>NEUST</title>
 </head>
+
 <body>
- 
+
     <style>
-        .navbar{
-          background-color: transparent;
+        .navbars {
+            background: rgba(255, 255, 255, 0.15);
+            /* box-shadow: 0 8px 7px 0 rgb(255,255,255); */
+            backdrop-filter: blur(20px);
+            -webkit-backdrop-filter: blur(6.5px);
+            border-radius: 10px;
+            border: 1px solid rgba(255, 255, 255, 0.18);
+            height: 90px;
+
         }
-    
+        
+
         /* .navbar-nav .nav-link {
             color: #ffffff; 
         } */
 
-        .navbar-nav .nav-link:hover{
+        .navbar-nav .nav-link:hover {
             color: #e9ecef;
         }
 
-        .l{
+        .l {
             color: #1d3698;
         }
 
@@ -43,15 +54,15 @@
             font-weight: bold;
         }
 
-        .navbar-brand:hover{
+        .navbar-brand:hover {
             color: #ffffff;
         }
-        
-        .navbar-toggler-icon{
-          color: #ffffff;
-        }        
 
-        .bt{
+        .navbar-toggler-icon {
+            color: #ffffff;
+        }
+
+        .bt {
             color: #ffffff;
             width: 82px;
             padding: 8px;
@@ -61,7 +72,7 @@
             text-align: center;
         }
 
-        .bx{
+        .bx {
             color: #ffffff;
             width: 82px;
             padding: 8px;
@@ -70,7 +81,7 @@
             text-align: center;
         }
 
-        .bc{
+        .bc {
             color: #ffffff;
             width: 82px;
             padding: 8px;
@@ -82,12 +93,25 @@
     </style>
 
     <!-- Start Navigation -->
-    <nav class="navbar navbar-expand-md fixed-top">
-       
-                <?php 
-                    session_start();   
-                    if (isset($_SESSION['is_login'])){
-                        echo '<a href="index.php" class="navbar-brand"><img src="https://neust.edu.ph/wp-content/uploads/2020/06/neust_logo-1.png" width="70px" />    <span style="color:#1d3698;">NEUST</span></a>
+    <nav class=" navbars navbar-expand fixed-top">
+
+        <?php
+        session_start();
+        if (isset($_SESSION['is_login'])) {
+            echo '<a href="index.php" class="navbar-brand pl-2"><img src="https://neust.edu.ph/wp-content/uploads/2020/06/neust_logo-1.png" width="70px" />    <span style="color:#1d3698;">NEUST</span></a>
+                        <!-- <span class="navbar-text" style="color: #ffffff;">Learn and Implement</span> -->
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myMenu" aria-controls="myMenu" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon" style="color:#1d3698;">=</span>
+                        </button>
+                        
+                        <div class="collapse navbar-collapse" id="myMenu">
+                            <ul class="navbar-nav ml-auto">';
+
+            echo '<li class="nav-item" style="position:relative; bottom:60px;"><a href="student/studentProfile.php" class="nav-link l">My Profile</a></li>';
+            echo '<li class="nav-item pr-2" style="position:relative; bottom:60px;"><a href="logout.php" class="nav-link l">Logout</a></li>';
+        } else {
+
+            echo '<a href="index.php" class="navbar-brand"><img src="https://neust.edu.ph/wp-content/uploads/2020/06/neust_logo-1.png" width="70px" />  NEUST</a>
                         <!-- <span class="navbar-text" style="color: #ffffff;">Learn and Implement</span> -->
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myMenu" aria-controls="myMenu" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon">=</span>
@@ -95,25 +119,12 @@
                         
                         <div class="collapse navbar-collapse" id="myMenu">
                             <ul class="navbar-nav ml-auto">';
-                            
-                        echo '<li class="nav-item"><a href="student/studentProfile.php" class="nav-link l">My Profile</a></li>';
-                        echo '<li class="nav-item"><a href="logout.php" class="nav-link l">Logout</a></li>';
-                    } else {
 
-                        echo '<a href="index.php" class="navbar-brand"><img src="https://neust.edu.ph/wp-content/uploads/2020/06/neust_logo-1.png" width="70px" />  NEUST</a>
-                        <!-- <span class="navbar-text" style="color: #ffffff;">Learn and Implement</span> -->
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myMenu" aria-controls="myMenu" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon">=</span>
-                        </button>
-                        
-                        <div class="collapse navbar-collapse" id="myMenu">
-                            <ul class="navbar-nav ml-auto">';
-
-                        echo '<li class="nav-item"><a href="#login" class="bt nav-link" data-toggle="modal" data-target="#stuLoginModalCenter">Login</a></li>';
-                        echo '<li class="nav-item"><a href="#signup" class="bx nav-link" data-toggle="modal" data-target="#stuRegModalCenter">Signup</a></li>';     
-                    }
-                ?> 
-            </ul>
+            echo '<li class="nav-item"><a href="#login" class="bt nav-link" data-toggle="modal" data-target="#stuLoginModalCenter">Login</a></li>';
+            echo '<li class="nav-item"><a href="#signup" class="bx nav-link" data-toggle="modal" data-target="#stuRegModalCenter">Signup</a></li>';
+        }
+        ?>
+        </ul>
         </div>
     </nav>
     <!-- End Navigation -->
@@ -121,11 +132,12 @@
 
     <!-- Include Bootstrap JS and jQuery -->
     <!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
 
-<!-- Bootstrap Bundle (includes Popper) -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap Bundle (includes Popper) -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
