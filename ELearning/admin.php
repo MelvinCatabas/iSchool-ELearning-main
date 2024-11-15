fb4ae809d23b32cb1c77c910f6ac10c52f762f32
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2024-11-15 22:24:34 +0800

    Updating Admin Home

--- ELearning/admin.php
+++ ELearning/admin.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION['is_login'])) {
               <div class="vid-content pl-3">
                 <h1 class="my-content">Welcome to <b>NEUST</b></h1>
                 <small class="my-content" style="font-size:16px;">Learn and Implement</small><br />';
-    echo '<a class="btn btns mt-3" href="#" data-toggle="modal" data-target="#adminLoginModalCenter">Get Started</a>';
+    echo '<a class="btn btns mt-3" href="#" data-toggle="modal" data-target="#adminLoginModalCenter">Admin Login</a>';
     echo '</div>
           </div>';
 }
