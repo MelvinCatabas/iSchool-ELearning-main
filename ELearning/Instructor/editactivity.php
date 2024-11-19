19c06687a70e468587d192a2d92695835a0316ef
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2024-11-19 15:38:28 +0800

    Adding Activity Page

---
+++ ELearning/Instructor/editactivity.php
@@ -0,0 +1,81 @@
+<?php 
+if(!isset($_SESSION)){ 
+  session_start(); 
+}
+define('TITLE', 'Edit Lesson');
+define('PAGE', 'edit lesson');
+include('./instructorInclude/header.php'); 
+include('../dbConnection.php');
+
+ if(isset($_SESSION['is_instructor_login'])){
+  $adminEmail = $_SESSION['instructorLogEmail'];
+ } else {
+  echo "<script> location.href='../index.php'; </script>";
+ }
+ // Update
+ if(isset($_REQUEST['requpdate'])){
+  // Checking for Empty Fields
+  if(($_REQUEST['activity_id'] == "") || ($_REQUEST['activity_title'] == "") ||  ($_REQUEST['course_id'] == "")){
+   // msg displayed if required field missing
+   $msg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert"> Fill All Fileds </div>';
+  } else {
+    // Assigning User Values to Variable
+    $activity_id = $_REQUEST['activity_id'];
+    $activity_title = $_REQUEST['activity_title'];
+    $llink = '../activitylink/'. $_FILES['activity_link']['name'];
+    $cid = $_REQUEST['course_id'];
+    // $cname = $_REQUEST['course_name'];
+    
+    
+   $sql = "UPDATE activity SET activity_id = '$activity_id', activity_title = '$activity_title', activity_link = '$llink', course_id='$cid' WHERE activity_id = '$activity_id'";
+    if($conn->query($sql) == TRUE){
+     // below msg display on form submit success
+     $msg = '<div class="alert alert-success col-sm-6 ml-5 mt-2" role="alert"> Updated Successfully </div>';
+    } else {
+     // below msg display on form submit failed
+     $msg = '<div class="alert alert-danger col-sm-6 ml-5 mt-2" role="alert"> Unable to Update </div>';
+    }
+  }
+  }
+ ?>
+<div class="col-sm-6 mt-5  mx-3 jumbotron">
+  <h3 class="text-center">Update Activity Details</h3>
+  <?php
+ if(isset($_REQUEST['view'])){
+  $sql = "SELECT * FROM activity WHERE activity_id = {$_REQUEST['id']}";
+ $result = $conn->query($sql);
+ $row = $result->fetch_assoc();
+ }
+ ?>
+  <form action="" method="POST" enctype="multipart/form-data">
+    <div class="form-group">
+      <label for="activity_id">Activity ID</label>
+      <input type="text" class="form-control" id="activity_id" name="activity_id" value="<?php if(isset($row['activity_id'])) {echo $row['activity_id']; }?>" readonly>
+    </div>
+    <div class="form-group">
+      <label for="activity_title">Activity Title</label>
+      <input type="text" class="form-control" id="activity_title" name="activity_title" value="<?php if(isset($row['activity_title'])) {echo $row['activity_title']; }?>">
+    </div>
+
+    <div class="form-group">
+      <label for="activity_link">Activity Link</label>
+      <input type="file" class="form-control" id="activity_link" name="activity_link" value="<?php if(isset($row['activity_link'])) {echo $row['activity_link']; }?>">
+    </div>
+    <div class="form-group">
+      <label for="course_id">Course ID</label>
+      <input type="text" class="form-control" id="course_id" name="course_id" value="<?php if(isset($row['course_id'])) {echo $row['course_id']; }?>" readonly>
+    </div>
+
+    <div class="text-center">
+      <button type="submit" class="btn btn-danger" id="requpdate" name="requpdate">Update</button>
+      <a href="activity.php" class="btn btn-secondary">Close</a>
+    </div>
+    <?php if(isset($msg)) {echo $msg; } ?>
+  </form>
+</div>
+</div>  <!-- div Row close from header -->
+</div>  <!-- div Conatiner-fluid close from header -->
+
+<?php
+  include('./instructorInclude/footer.php');
+?>
--- ELearning/Student/watchcourse.php
+++ ELearning/Student/watchcourse.php
@@ -19,32 +19,23 @@ if (isset($_SESSION['is_login'])) {
    <title>
       NEUST
    </title>
-   <!--     Fonts and icons     -->
+   <!-- Fonts and icons -->
    <link href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,800" rel="stylesheet" />
-   <!-- Nucleo Icons -->
    <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-icons.css" rel="stylesheet" />
    <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-svg.css" rel="stylesheet" />
-   <!-- Font Awesome Icons -->
    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-   <!-- CSS Files -->
    <link id="pagestyle" href="../css/soft-ui-dashboard.css?v=1.1.0" rel="stylesheet" />
-   <!-- Nepcha Analytics (nepcha.com) -->
-   <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
    <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 </head>
 
 <body>
 
-
    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
       <div class="container-fluid py-1 px-3">
          <nav aria-label="breadcrumb">
-
             <h6 class="font-weight-bolder mb-0">Profile Dashboard</h6>
          </nav>
          <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-
-
          </div>
       </div>
    </nav>
@@ -63,40 +54,58 @@ if (isset($_SESSION['is_login'])) {
                   $result = $conn->query($sql);
                   if ($result->num_rows > 0) {
                      while ($row = $result->fetch_assoc()) {
-                        echo ' <li class="nav-item border-bottom py-2  " movieurl=' . $row['lesson_link'] . ' style="cursor:pointer;">
-        
-      
-             <span class="nav-link-text ms-1">' . $row['lesson_name'] . '</span>
-         
-         </li>
-          <hr class="horizontal dark mt-0">';
+                        echo '<li class="nav-item border-bottom py-2" movieurl="' . $row['lesson_link'] . '" style="cursor:pointer;">
+                                 <span class="nav-link-text ms-1">' . $row['lesson_name'] . '</span>
+                              </li>
+                              <hr class="horizontal dark mt-0">';
                      }
                   }
                }
                ?>
             </ul>
+
+            <h4 class="text-center">Activity</h4>
+            <ul id="playlist" class="nav navbar-nav flex-column">
+               <?php
+               if (isset($_GET['course_id'])) {
+                  $course_id = $_GET['course_id'];
+                  $sql = "SELECT * FROM activity WHERE course_id = '$course_id'";
+                  $result = $conn->query($sql);
+                  if ($result->num_rows > 0) {
+                     while ($row = $result->fetch_assoc()) {
+                        echo '<li class="nav-item border-bottom py-2">
+                                 <a href="' . $row['activity_link'] . '" target="_blank" style="text-decoration: none;" class="d-flex align-items-center">
+                                    <span class="nav-link-text ms-1">' . $row['activity_title'] . '</span>
+                                 </a>
+                              </li>
+                              <hr class="horizontal dark mt-0">';
+                     }
+                  }
+               }
+               ?>
+            </ul>
+
+
          </div>
          <div class="col-sm-8">
-            <video id="videoarea" src="" class="mt-5 w-85 ml-2" style="margin-left: 64px;" controls>
-            </video>
+            <video id="videoarea" src="" class="mt-5 w-85 ml-2" style="margin-left: 64px;" controls></video>
          </div>
       </div>
    </div>
 
+   <!-- <div class="container-fluid">
+      <div class="row">
+         <div class="col-sm-3 border-right">
+           
+         </div>
+      </div>
+   </div> -->
 
-
-   <!-- Jquery and Boostrap JavaScript -->
+   <!-- Scripts -->
    <script type="text/javascript" src="../js/jquery.min.js"></script>
    <script type="text/javascript" src="../js/popper.min.js"></script>
    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
-
-   <!-- Font Awesome JS -->
    <script type="text/javascript" src="../js/all.min.js"></script>
-
-   <!-- Ajax Call JavaScript -->
-   <!-- <script type="text/javascript" src="..js/ajaxrequest.js"></script> -->
-
-   <!-- Custom JavaScript -->
    <script type="text/javascript" src="../js/custom.js"></script>
 </body>
 
